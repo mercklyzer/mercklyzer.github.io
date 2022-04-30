897c1dd2d1347aa2d5b2e6d765a589667bdaa429
mercklyzer <mercklyzer@gmail.com>
2022-04-30 15:53:01 +0800

    Fix with gsap

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import NeonAnimate from '../assets/neon-animate.gif'
+import gsap from "gsap";
 
 const Intro = props => {
-    const [show, setShow] = useState(true)
     const [behind, setBehind] = useState(false)
     const headRef = useRef()
 
@@ -10,23 +10,32 @@ const Intro = props => {
     useEffect(() => {
         document.querySelector("body").classList.add("disable-scroll")
 
-        new Promise((fulfill, reject) => {
-            setTimeout(() => {
+        gsap.to(headRef.current, {
+            opacity: 0,
+            duration: 1,
+            delay: 4.7,
+            onComplete: () => {
                 document.querySelector("body").classList.remove("disable-scroll")
-                
-                fulfill()
-            },5000)
-        })
-        .then(() => {
-            setShow(false)
-            setTimeout(() => {
                 setBehind(true)
-            }, 1000)
-        })        
+            }
+        })
+
+        // new Promise((fulfill, reject) => {
+        //     setTimeout(() => {
+        //         document.querySelector("body").classList.remove("disable-scroll")
+                
+        //         fulfill()
+        //     },5000)
+        // })
+        // .then(() => {
+        //     setTimeout(() => {
+        //         setBehind(true)
+        //     }, 1000)
+        // })        
     }, [])
 
     return (
-        <div ref={headRef} className={`bg-black h-screen w-screen fixed top-0 left-0 z-[999] ${behind? 'hidden':''} opacity-${show? '1':'0'} duration-[1000ms]`}>
+        <div ref={headRef} className={`bg-black h-screen w-screen fixed top-0 left-0 z-[999] ${behind? 'hidden':''} opacity-1`}>
             <div 
                 className="flex justify-center items-center h-full">
                 <img src={NeonAnimate} className="w-[80vw] md:w-[70vw] lg:w-[40vw] h-auto object-cover"/>
--- src/index.js
+++ src/index.js
@@ -6,19 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
