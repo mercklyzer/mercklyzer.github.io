edb7bca41ee28d4202db18b786b30a88b9245dc6
mercklyzer <mercklyzer@gmail.com>
2022-05-04 15:57:10 +0800

    Change navbar to absolute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ const Navbar = props => {
     }, [])
 
     return (
-    <div className="px-8 fixed left-0 top-0 pt-6 w-full flex justify-start items-center z-10 mix-blend-difference text-silver ">
-        <div onClick={() => window.location.replace('#home')} className="flex flex-col font-maven duration-500 group hover:text-neon cursor-pointer" >
+    <div className="px-8 absolute left-0 top-0 pt-6 w-full flex justify-start items-center z-10 text-neon ">
+        <div onClick={() => window.location.replace('#home')} className="flex flex-col font-maven duration-500 group cursor-pointer" >
             <div ref={brandRef} className="font-bold text-md lg:text-xl lg:tracking-wider nav-item group-hover:tracking-widest duration-500">LYZER BAUTISTA</div>
             <div ref={positionRef} className="font-semibold text-sm lg:text-md nav-item duration-300 ">Software Engineer</div>
         </div>
