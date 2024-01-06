ac013f15ccadb86cbafd4c21b05b927f9d4ddc0b
Lyzer Merck Bautista <66999325+mercklyzer@users.noreply.github.com>
2024-01-06 15:33:08 +0800

    feat: update visit link, blogs, and resume

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,49 +1,81 @@
-import React from "react"
+import React from "react";
 
-const Preview = ({title, image, preview, visitLink}) => {
-    if(visitLink){
-        return (<a href={visitLink}
-            target="__blank"
-            className="group cursor-pointer">
-            <img className="object-contain group-hover:hidden" src={image} alt={`${title} preview`}/>
-            <img className="object-contain hidden group-hover:block" src={preview} alt={`${title} preview`}/>
-        </a>)
-    }
-
+const Preview = ({ title, image, preview, visitLink }) => {
+  if (visitLink) {
     return (
-        <div
-            className="group disabled">
-            <img className="object-contain group-hover:hidden" src={image} alt={`${title} preview`}/>
-            <img className="object-contain hidden group-hover:block" src={preview} alt={`${title} preview`}/>
-        </div>
-    )
-}
+      <a href={visitLink} target="__blank" className="group cursor-pointer">
+        <img
+          className="object-contain group-hover:hidden"
+          src={image}
+          alt={`${title} preview`}
+        />
+        <img
+          className="object-contain hidden group-hover:block"
+          src={preview}
+          alt={`${title} preview`}
+        />
+      </a>
+    );
+  }
 
-const Visit = ({visitLink}) => {
-    if(visitLink){
-        return (
-            <a href={visitLink} target="__blank" className="text-sm font-medium cursor-pointer hover:text-neon duration-500 block">Visit</a>
-        )
-    }
-
-    return (
-        <span className="text-sm font-medium  opacity-60 block">Visit</span>
-    )
-}
-
-const Project = ({image, title, visitLink, preview, documentationLink}) => {
-    return (
-    <div className="project flex flex-col justify-center items-center w-full opacity-0 ">
-        <Preview title={title} image={image} preview={preview} visitLink={visitLink}/>
-        <div className="flex justify-between items-center w-full  text-white mt-2 font-poppins">
-            <div className="font-semibold text-md">{title}</div>
-            <div>
-                <Visit visitLink={visitLink}/>
-                {documentationLink && <a href={documentationLink} target="__blank" className="text-sm font-medium cursor-pointer hover:text-neon duration-500 block">Documentation</a>}
-            </div>
-        </div>
+  return (
+    <div className="group disabled">
+      <img
+        className="object-contain group-hover:hidden"
+        src={image}
+        alt={`${title} preview`}
+      />
+      <img
+        className="object-contain hidden group-hover:block"
+        src={preview}
+        alt={`${title} preview`}
+      />
     </div>
-    )
-}
+  );
+};
 
-export default Project
+const Visit = ({ visitLink }) => {
+  if (visitLink) {
+    return (
+      <a
+        href={visitLink}
+        target="__blank"
+        className="text-sm font-medium cursor-pointer hover:text-neon duration-500 block"
+      >
+        Visit
+      </a>
+    );
+  }
+
+  return <span className="text-sm font-medium  opacity-60 block">Visit</span>;
+};
+
+const Project = ({ image, title, visitLink, preview, documentationLink }) => {
+  return (
+    <div className="project flex flex-col justify-center items-center w-full opacity-0 ">
+      <Preview
+        title={title}
+        image={image}
+        preview={preview}
+        visitLink={visitLink}
+      />
+      <div className="flex justify-between items-center w-full  text-white mt-2 font-poppins">
+        <div className="font-semibold text-md">{title}</div>
+        <div>
+          {visitLink && <Visit visitLink={visitLink} />}
+          {documentationLink && (
+            <a
+              href={documentationLink}
+              target="__blank"
+              className="text-sm font-medium cursor-pointer hover:text-neon duration-500 block"
+            >
+              Documentation
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,47 +1,109 @@
 import React, { useState } from "react";
 
-const Sidebar = props => {
-    const [open, setOpen] = useState(false)
+const Sidebar = (props) => {
+  const [open, setOpen] = useState(false);
 
-    const toggleOpen = () => {
-        document.querySelector("body").classList.toggle("disable-scroll")
-        setOpen((val) => !val)
-    }
+  const toggleOpen = () => {
+    document.querySelector("body").classList.toggle("disable-scroll");
+    setOpen((val) => !val);
+  };
 
-    const handleNavigate = (id) => {
-        setOpen(false)
-        document.querySelector("body").classList.toggle("disable-scroll")
-        window.location.replace(id)
-    }
+  const handleNavigate = (id) => {
+    setOpen(false);
+    document.querySelector("body").classList.toggle("disable-scroll");
+    window.location.replace(id);
+  };
 
-    return (
-        <div className={`section z-10 sidebar ${open? 'open' : ''}`}>
-            <div className="menu bg-neon opacity-1 border-black border" onClick={toggleOpen}>
-                {!open && <svg className="stroke-black" fill="none" stroke="" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>}
-                {open && <svg className="stroke-black" fill="none" stroke="" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>}
-            </div>
+  return (
+    <div className={`section z-10 sidebar ${open ? "open" : ""}`}>
+      <div
+        className="menu bg-neon opacity-1 border-black border"
+        onClick={toggleOpen}
+      >
+        {!open && (
+          <svg
+            className="stroke-black"
+            fill="none"
+            stroke=""
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        )}
+        {open && (
+          <svg
+            className="stroke-black"
+            fill="none"
+            stroke=""
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        )}
+      </div>
 
-            <div className="flex flex-col justify-center w-full h-full">
-
-                <div className="">
-                    <span className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer" onClick={() => handleNavigate("#home")}> Home </span>
-                </div>
-                <div className="">
-                    <span className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer" onClick={() => handleNavigate("#about")}> About </span>
-                </div>
-                <div className="">
-                    <span className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer" onClick={() => handleNavigate("#projects")}> Projects </span>
-                </div>
-                <div className="">
-                    <a className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer" href="https://blogs.lyzerbautista.com/"> Blogs </a>
-                </div>
-                <div className="">
-                    <span className="font-monument text-4xl lg:text-6xl cursor-pointer" onClick={() => handleNavigate("#contact")}> Contact </span>
-                </div>
-
-            </div>
+      <div className="flex flex-col justify-center w-full h-full">
+        <div className="">
+          <span
+            className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer"
+            onClick={() => handleNavigate("#home")}
+          >
+            {" "}
+            Home{" "}
+          </span>
         </div>
-    )
-}
+        <div className="">
+          <span
+            className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer"
+            onClick={() => handleNavigate("#about")}
+          >
+            {" "}
+            About{" "}
+          </span>
+        </div>
+        <div className="">
+          <span
+            className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer"
+            onClick={() => handleNavigate("#projects")}
+          >
+            {" "}
+            Projects{" "}
+          </span>
+        </div>
+        <div className="">
+          <a
+            className="font-monument text-4xl lg:text-6xl mb-6 lg:mb-12 inline-block cursor-pointer"
+            href="https://blogs.lyzerbautista.com/"
+            target="__blank"
+          >
+            {" "}
+            Blogs{" "}
+          </a>
+        </div>
+        <div className="">
+          <span
+            className="font-monument text-4xl lg:text-6xl cursor-pointer"
+            onClick={() => handleNavigate("#contact")}
+          >
+            {" "}
+            Contact{" "}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
